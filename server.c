277ebbbf8865a9ea0013a0190fa4392be6e7dbca
hasu <jun2077681@naver.com>
2018-12-06 17:58:28 +0900

    linked list test

--- server.c
+++ server.c
@@ -7,18 +7,40 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 #define BUFF_SIZE 1024
+#define THREAD_NUM 10
+
+typedef struct _node{
+	int value;
+	struct _node* next;
+}node;
+
+typedef node* nptr;
+
+typedef struct _list{
+	int count;
+	nptr head;
+}list;
+
+list* init(void);
+void insert(list* lptr, int value);
+void delete(list* lptr, int value);
 
 int cntNum = 0;
+
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 void *thrfunc(void *arg);
 
 int main()
 {
 	int server_socket, client_socket;
-	int accp_sock[10];
+	int accp_sock[THREAD_NUM];
 	struct sockaddr_in server_addr, client_addr;
+	pthread_t tid[THREAD_NUM];
+	int status;
 
 	char buff_rcv[BUFF_SIZE+5];
 	char buff_snd[BUFF_SIZE+5];
@@ -82,4 +104,67 @@ void *thrfunc(void *arg) {
 	write(accp_sock, &result, 4);
 
 	close(accp_sock);
+}
+
+list* init(void)
+{
+	list* mylist = (list*)malloc(sizeof(list));
+	mylist->count = 0;
+	mylist->head = NULL;
+
+	return mylist;
+}
+
+void insert(list* lptr, int value)
+{
+	nptr new_nptr = (node*)malloc(sizeof(node));
+	new_nptr->value = value;
+	new_nptr->next = NULL;
+
+	if(lptr->head == NULL)
+	{
+		lptr->head = new_nptr;
+	}
+	else
+	{
+		nptr tail = lptr->head;
+		while(tail->next != NULL)
+		{
+			tail = tail->next;
+		}
+		tail->next = new_nptr;
+	}
+	lptr->count++;
+}
+void delete(list* lptr, int value)
+{
+	if(lptr->count == 0) return;
+	if(lptr->count == 1)
+	{
+		if(lptr->head->value == value)
+		{
+			nptr tmp = lptr->head;
+			lptr->head = NULL;
+			free(tmp);
+			lptr->count--;
+		}
+		else
+		{
+			return;
+		}
+	}
+	nptr cur = lptr->head;
+
+	while(cur != NULL && cur->next!=NULL && cur->next->value != value)
+	{
+		cur = cur->next;
+	}
+	if(cur == NULL || cur->next == NULL) return;
+
+	nptr tmp = cur->next;
+	cur->next = tmp->next;
+	free(tmp);
+	lptr->count--;
+
+	return;
 }
