36c34e90f8b49399051672fbecf21995545046d0
hasu <jun2077681@naver.com>
2018-12-09 21:21:17 +0900

    signal control

--- client.c
+++ client.c
@@ -8,14 +8,38 @@
 #include <arpa/inet.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <signal.h>
 
 #define BUFF_SIZE 1024
+
+void *thrfunc(void* arg);
+
+struct Info{
+	int fd;
+	bool* terminate;
+};
+
+struct RecvPacket{
+	int client_num;
+	char buf[BUFF_SIZE];
+};
+
+typedef struct Info info;
+typedef struct RecvPacket recv_packet;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+int fd;
 
 int main()
 {
 	struct sockaddr_in s;
 	bool terminate = false;
 	char buf[BUFF_SIZE];
+	int status;
+	pthread_t tid;
+
+	signal(SIGINT, SIG_IGN);
+	signal(SIGSTOP, SIG_IGN);
+	signal(SIGQUIT, SIG_IGN);
 
 	bzero((char*)&s, sizeof(s));
 
@@ -23,7 +47,7 @@ int main()
 	s.sin_addr.s_addr = inet_addr("127.0.0.1");
 	s.sin_port = htons(1234);
 
-	int fd = socket(PF_INET, SOCK_STREAM, 0);
+	fd = socket(PF_INET, SOCK_STREAM, 0);
 	if(fd < 0)
 	{
 		perror("simplex-talk: socket");
@@ -36,14 +60,43 @@ int main()
 		exit(1);
 	}
 
+	info server_info;
+	server_info.fd = fd;
+	server_info.terminate = &terminate;
+
+	if((status = pthread_create(&tid, NULL, thrfunc, (void*)&server_info)) != 0) {
+		printf("%lu thread create error: %s\n", tid, strerror(status));
+		exit(0);
+	}
+
 	while(!terminate)
 	{
-		read(0, buf, sizeof(buf));
-		if(!strcmp(buf, "Q"))
+		fgets(buf, sizeof(buf), stdin);
+
+		if(!strcmp(buf, "Q\n"))
 		{
 			terminate = true;
 		}
-		write(fd, buf, sizeof(buf));
+		send(fd, buf, sizeof(buf), 0);
 	}
 
+	close(fd);
+
+	return 0;
+}
+
+void *thrfunc(void* arg)
+{
+	info* server_info = (info*)arg;
+
+	int fd = server_info->fd;
+	recv_packet* packet = (recv_packet*)malloc(sizeof(recv_packet));
+
+	while(!*server_info->terminate)
+	{
+		recv(fd, packet, sizeof(recv_packet), 0);
+		printf("%d Client : %s\n", packet->client_num, packet->buf);
+	}
+
+	return NULL;
 }
--- server.c
+++ server.c
@@ -8,6 +8,7 @@
 #include <arpa/inet.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <signal.h>
 
 #define BUFF_SIZE 1024
 
@@ -17,12 +18,19 @@ struct listNode {
 	struct listNode *nextPtr;
 };
 
+struct RecvPacket{
+	int client_num;
+	char buf[BUFF_SIZE];
+};
+
 typedef struct listNode ListNode;
+typedef struct RecvPacket recv_packet;
 typedef ListNode *ListNodePtr;
 
 ListNodePtr insert(ListNodePtr *sPtr, int value);
 void delete(ListNodePtr *sPtr, int value);
 bool isEmpty(ListNodePtr sPtr);
+void printList(ListNodePtr currentPtr);
 
 void *thrfunc(void* arg);
 
@@ -32,11 +40,15 @@ pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 int main()
 {
-	int server_socket, client_socket;
+	int server_socket;
 	int sock;
 
 	struct sockaddr_in server_addr, client_addr;
 	int status;
+
+	signal(SIGINT, SIG_IGN);
+	signal(SIGSTOP, SIG_IGN);
+	signal(SIGQUIT, SIG_IGN);
 
 	bzero((char*)&server_addr, sizeof(server_addr));
 	bzero((char*)&client_addr, sizeof(client_addr));
@@ -58,34 +70,57 @@ int main()
 		exit(1);
 	}
 
+	if(listen(server_socket, 10) < 0)
+	{
+		perror("Server : Can't listening connect.");
+		exit(1);
+	}
+
 	while(1)
 	{
-		if(listen(server_socket, 10) < 0)
-		{
-			perror("Server : Can't listening connect.");
-			exit(1);
-		}
-
 		int addr_size = sizeof(client_addr);
 		int sock = accept(server_socket, (struct sockaddr*)&client_addr, &addr_size);
-		ListNodePtr socknode = insert(&accp_sock, sock);
 
-		if(client_socket < 0)
+		if(server_socket < 0)
 		{
 			perror("Server: accept failed.");
 			exit(1);
 		}
+
+		ListNodePtr socknode = insert(&accp_sock, sock);
+
+		ListNodePtr curPtr = accp_sock;
+		
+		recv_packet* packet = (recv_packet*)malloc(sizeof(recv_packet));
+		packet->client_num = socknode->data;
+
+		while(curPtr != NULL)
+		{
+			if(curPtr->data == socknode->data)
+			{
+				curPtr = curPtr->nextPtr;
+				continue;
+			}
+			sprintf(packet->buf, "Client %d has been connected!\n", socknode->data);
+
+			send(curPtr->data, packet, sizeof(recv_packet), 0);
+			printf("-------------------------------------\n");
+			printf("send to %d\n", curPtr->data);
+			printf("send data : %s\n", packet->buf);
+			printf("-------------------------------------\n");
+
+			curPtr = curPtr->nextPtr;
+		}
+
 		if((status = pthread_create(&(socknode->tid), NULL, thrfunc, (void*)socknode)) != 0) {
 			printf("%lu thread create error: %s\n", socknode->tid, strerror(status));
 			exit(0);
 		}
-
-		pthread_join(socknode->tid, NULL);
-		delete(&accp_sock, sock);
+		pthread_detach(socknode->tid);
 	}
 }
 
-void *thrfunc(void* arg) 
+void *thrfunc(void* arg)
 {
 	ListNodePtr socknode = (ListNodePtr)arg;
 	int sock = socknode->data;
@@ -94,26 +129,47 @@ void *thrfunc(void* arg)
 
 	while(!terminate)
 	{
-		read(sock, buf, sizeof(buf));
+		recv(sock, buf, sizeof(buf), 0);
 		printf("client %d send value = %s\n", sock, buf);
 
 		pthread_mutex_lock(&lock);
 
 		ListNodePtr curPtr = accp_sock;
+
+		recv_packet* packet = (recv_packet*)malloc(sizeof(recv_packet));
+		packet->client_num = sock;
+
 		while(curPtr != NULL)
 		{
-			if(curPtr->data == sock) continue;
-			write(curPtr->data, buf, sizeof(buf));
+			if(curPtr->data == sock)
+			{
+				curPtr = curPtr->nextPtr;
+				continue;
+			}
+
+			memcpy(packet->buf, buf, sizeof(buf));
+
+			send(curPtr->data, packet, sizeof(recv_packet), 0);
+			printf("-------------------------------------\n");
+			printf("send to %d\n", curPtr->data);
+			printf("send data : %s\n", packet->buf);
+			printf("-------------------------------------\n");
+
 			curPtr = curPtr->nextPtr;
 		}
 
 		pthread_mutex_unlock(&lock);
-		if(!strcmp(buf, "Q"))
+
+		if(!strcmp(buf, "Q\n"))
 		{
 			terminate = true;
 		}
 	}
 	close(sock);
+
+	delete(&accp_sock, sock);
+
+	return NULL;
 }
 
 ListNodePtr insert(ListNodePtr *sPtr, int value)
@@ -195,4 +251,22 @@ void delete(ListNodePtr *sPtr, int value)
 bool isEmpty(ListNodePtr sPtr)
 {
 	return sPtr == NULL;
+}
+
+void printList(ListNodePtr currentPtr)
+{
+	if(currentPtr == NULL)
+	{
+		printf("empty.\n");
+	}
+	else
+	{
+		printf("List : \n");
+		while(currentPtr != NULL)
+		{
+			printf("fd : %d tid : %lu\n", currentPtr->data, currentPtr->tid);
+			currentPtr = currentPtr->nextPtr;
+		}
+		printf("NULL\n\n");
+	}
 }
